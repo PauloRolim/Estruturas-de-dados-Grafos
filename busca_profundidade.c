89c6b32b091896b047b0b925773d55e21740bc24
PauloRolim <pauloerb@hotmail.com>
2021-08-15 10:56:03 -0300

    testes

--- busca_profundidade.c
+++ busca_profundidade.c
@@ -14,16 +14,67 @@ typedef int TIPOPESO;
 #define AMARELO 1
 #define VERMELHO 2
 
-typedef struct adjacencia
+typedef struct adjacencia //na verdade essa estrutura é uma lista ligada
 {
-    int vertice;
+    int vertice; //vertice de destino da adjacencia
     TIPOPESO peso;
-    struct adjacencia *prox;
+    struct adjacencia *prox; //próximo vértice da lista
 } ADJACENCIA;
 
 typedef struct vertice
 {
     /* inserir outros dados necessários */
-    ADJACENCIA *cab;
+    ADJACENCIA *cab; // o vértice só tem a cabeça da sua lista de adjacências, suas arestas
 
 } VERTICE;
+
+typedef struct grafo
+{
+    int vertices;
+    int arestas;
+    VERTICE *adj;
+} GRAFO;
+
+void profundidade(GRAFO *g)
+{
+    int cor[g->vertices]; /*arranjo de cores para colorir os vertices visitados.
+                            o índice do vertice no arranjo adj em GRAFO deve coincidir 
+                            com o arranjo de cores, para possibilitar o relacionamento
+                            entre a cor designada e o vértice do grafo*/
+
+    int u;
+
+    for (u = 0; u < g->vertices; u++)
+    {
+        cor[u] = BRANCO;
+    }
+
+    for (u = 0; u < g->vertices; u++)
+    {
+        if (cor[u] == BRANCO)
+        {
+            visitaP(g,u,cor);
+        }
+        
+    }
+        
+}
+
+void visitaP(GRAFO *g, int u, int *cor)
+{
+    cor[u] = AMARELO;
+    ADJACENCIA *v = g->adj[u].cab;
+
+    while (v)
+    {
+        if (cor[v->vertice] == BRANCO)
+        {
+            visitaP(v,g->vertices,cor);
+            v = v->prox;
+        }
+        cor[u] = VERMELHO;
+    }
+    
+
+}
+

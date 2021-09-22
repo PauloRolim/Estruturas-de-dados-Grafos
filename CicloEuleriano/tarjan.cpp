26d2e492b91eeee4195538a552a9395addb7430d
PauloRolim <pauloerb@hotmail.com>
2021-09-21 22:16:22 -0300

    comentários

--- CicloEuleriano/tarjan.cpp
+++ CicloEuleriano/tarjan.cpp
@@ -29,86 +29,97 @@ Graph::Graph(int V)
 void Graph::addEdge(int v, int w)
 {
     adj[v].push_back(w);
-    adj[w].push_back(v);  // Note: the graph is undirected
+    adj[w].push_back(v);  // O grafo é não direcionado
 }
  
-// A recursive function that finds and prints bridges using
-// DFS traversal
-// u --> The vertex to be visited next
-// visited[] --> keeps tract of visited vertices
-// disc[] --> Stores discovery times of visited vertices
-// parent[] --> Stores parent vertices in DFS tree
+/* 
+   ** Uma função recursiva que encontra e exibe na tela as pontes utilizando
+   ** travessia por meio de busca em profundidade - DFS.
+   ** u --> variável que diz o vértice que será visitado em seguida;
+   ** visited[] --> armazena os vértices visitados;
+   ** disc[] --> armazena o tempo de descoberta dos vértices visitados;
+   ** parent[] --> armazena os vértices adjacentes da árvore de busca em profundidade
+*/
 void Graph::bridgeUtil(int u, bool visited[], int disc[],
                                   int low[], int parent[])
 {
-    // A static variable is used for simplicity, we can
-    // avoid use of static variable by passing a pointer.
+    /* 
+      A static variable is used for simplicity, we can
+      avoid use of static variable by passing a pointer.
+    */
     static int time = 0;
  
-    // Mark the current node as visited
+    // Marca o vértice atual como visitado
     visited[u] = true;
  
-    // Initialize discovery time and low value
+    
+    // Inicializa o tempo de descoberta e o low value
     disc[u] = low[u] = ++time;
  
-    // Go through all vertices adjacent to this
+    
+    // Percorre todos os vértices adjacentes ao vértice fornecido
     list<int>::iterator i;
     for (i = adj[u].begin(); i != adj[u].end(); ++i)
     {
-        int v = *i;  // v is current adjacent of u
+        int v = *i;  // v é o atual adjacente a u
  
-        // If v is not visited yet, then recur for it
+        // Se v ainda não foi visitado, então retorna a ele
         if (!visited[v])
         {
             parent[v] = u;
             bridgeUtil(v, visited, disc, low, parent);
  
-            // Check if the subtree rooted with v has a
-            // connection to one of the ancestors of u
+                        
+            /*Verifica se a subarvore de raiz v está conectada
+            a um dos vértives antecedentes a u*/
             low[u]  = min(low[u], low[v]);
  
-            // If the lowest vertex reachable from subtree
-            // under v is  below u in DFS tree, then u-v
-            // is a bridge
+            /*Se o menor vértice alcançável pela subarvore
+             de raiz v está abaixo de u na árvore de DFS, então a aresta u-v
+             é uma ponte*/
             if (low[v] > disc[u])
-              cout << u <<" " << v << endl;
+            cout << u <<" " << v << endl;
         }
  
-        // Update low value of u for parent function calls.
+        // Atualiza o low value de u no vetor parent[]
         else if (v != parent[u])
             low[u]  = min(low[u], disc[v]);
     }
 }
  
-// DFS based function to find all bridges. It uses recursive
-// function bridgeUtil()
+
+/* Fução baseada na busca em profundidade para encontrar todas as pontes. Chama
+   recursivamente a fução bridgeUtil().
+*/
 void Graph::bridge()
 {
-    // Mark all the vertices as not visited
+
+    // Marca todos os vértices como não visitado
     bool *visited = new bool[V];
     int *disc = new int[V];
     int *low = new int[V];
     int *parent = new int[V];
  
-    // Initialize parent and visited arrays
+    
+    // Inicializa os vetores parent[] e visited[]
     for (int i = 0; i < V; i++)
     {
         parent[i] = NIL;
         visited[i] = false;
     }
  
-    // Call the recursive helper function to find Bridges
-    // in DFS tree rooted with vertex 'i'
+    //Chama recursivamente a função bridgeUtil() para encontrar as
+    // pontes na árvore de busca em profundidade com vértice em "i"
     for (int i = 0; i < V; i++)
         if (visited[i] == false)
             bridgeUtil(i, visited, disc, low, parent);
 }
  
-// Driver program to test above function
+
 int main()
 {
-    // Create graphs given in above diagrams
-    cout << "\nPontes no primeiro grafo \n";
+    
+    cout << "\nPontes no primeiro grafo " << endl;
     Graph g1(6);
     g1.addEdge(0, 1);
     g1.addEdge(0, 2);
@@ -118,7 +129,7 @@ int main()
     g1.addEdge(3, 4);
     g1.bridge();
  
-    cout << "\nPontes no segundo grafo \n";
+    cout << "\nPontes no segundo grafo " << endl;
     Graph g2(5);
     g2.addEdge(0, 1);
     g2.addEdge(0, 2);
@@ -127,7 +138,7 @@ int main()
     g2.addEdge(3, 4);
     g2.bridge();
  
-    cout << "\nPontes no terceiro grafo \n";
+    cout << "\nPontes no terceiro grafo " << endl;
     Graph g3(5);
     g3.addEdge(0, 1);
     g3.addEdge(0, 2);
